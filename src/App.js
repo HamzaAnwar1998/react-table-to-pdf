13533670e5cbdc1574e5552e70b186ac39b0ed8d
HamzaAnwar1998 <anwarhamza919@gmail.com>
2021-12-27 17:22:14 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import axios from 'axios'
+
+import jsPDF from 'jspdf';
+import 'jspdf-autotable'
+
 
 function App() {
+
+  // data state
+  const [data, setData]=useState([]);
+
+  // click on fetch api button
+  const fetchApi=()=>{
+    axios.get('https://jsonplaceholder.typicode.com/todos').then((response)=>{
+      const data = response.data;
+      setData(data);
+    })
+  }
+
+  // download data in pdf format
+  const downloadData=()=>{
+
+    const pdf = new jsPDF();
+    pdf.autoTable({html:'#table'})
+    pdf.save('Todos.pdf');
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='container'>
+
+      <br></br>
+      <h3>Simplest way to export HTML Table to PDF in React</h3>
+      
+      <button onClick={fetchApi} className='btn btn-success btn-md'>
+        FETCH API
+      </button>
+
+      {data.length > 0&&(
+
+        <div className='table-responsive' style={{marginTop:30+'px'}}>
+
+           <div className='download-data-div'>
+              <button className='btn btn-primary btn-md'
+                      onClick={downloadData}>
+                DOWNLOAD DATA
+              </button>
+           </div>
+
+           <h3>TODO LIST</h3>
+           
+           <table className='table table-borderless' id='table'>
+
+             <thead>
+               <tr>
+                 <th scope='col'>
+                   ID
+                 </th>
+                 <th scope='col'>
+                   TITLE
+                 </th>
+                 <th scope='col'>
+                   STATUS
+                 </th>
+               </tr>
+             </thead>
+
+             {data.map((individualData)=>(
+               <tbody key={individualData.id}>
+                 <tr>
+                   <td>{individualData.id}</td>
+                   <td>{individualData.title}</td>
+                   <td>{String(individualData.completed)}</td>
+                 </tr>
+               </tbody>
+             ))}
+
+           </table>
+
+        </div>
+
+      )}
+
     </div>
+
   );
+
 }
 
 export default App;
